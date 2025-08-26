eea1a4424ed9b12041c957f21bfbf3d185e18b24
Joseph Feest <josephfeest@outlook.com>
2025-08-26 22:44:25 +0100

    doc edits

--- R/plot_funcs.R
+++ R/plot_funcs.R
@@ -4,7 +4,7 @@
 #'
 #' @description Creates a network layout using the Fruchterman–Reingold force-directed layout algorithm, resulting in a balanced, spaced layout.
 #'
-#' @param network The network w
+#' @param network Single matrix input of the network for which the layout will be optimised. Function is designed for a network, NOT a precision matrix.
 #'
 #' @return Matrix array that gives the coordinates of each node. This should then be parsed as the layout_coords argument in create_network_plots().
 #' @export
@@ -26,35 +26,37 @@ create_layout <- function(network){
 
 
 
-#' Title
+#' @title create_network_plots
 #'
-#' @param data_list
-#' @param layout_coords
-#' @param color
-#' @param edge_color
-#' @param title
+#' @param net_list list of matrices of networks to plot
+#' @param layout_coords Matrix array of coordinates which should have been generated using the function `create_layout`.
+#' @param color Set colour of the nodes.
+#' @param edge_color Set colour of the edges
+#' @param title Set the title of the plot.
 #'
-#' @return
+#' @return Plots a 1xN array of networks, each with the same layout.
 #' @export
 #'
 #' @examples
-create_network_plots <- function(data_list, layout_coords, color = "red",edge_color = "purple",
+#' m <- matrix(sample(0:1, 25, replace=TRUE), nrow=5)
+#' m2 <- matrix(sample(0:1, 25, replace=TRUE), nrow=5)
+#' m3 <- matrix(sample(0:1, 25, replace=TRUE), nrow=5)
+#' m[lower.tri(m)] <- t(m)[lower.tri(m)]
+#' diag(m) <- 0
+#' layout <- create_layout(m)
+#' create_network_plots(list(m,m2,m3), layout_coords = layout)
+create_network_plots <- function(net_list, layout_coords, color = "red",edge_color = "purple",
                                  title = "Network Plots") {
-  N <- length(data_list)
+  N <- length(net_list)
   networks <- vector("list", N)
   plots <- vector("list", N)
 
-
-  # Loop to generate networks and their corresponding plots
   for (i in seq_len(N)) {
-    networks[[i]] <- network::network(data_list[[i]] > 0.5, directed = FALSE)
+    networks[[i]] <- network::network(net_list[[i]] > 0.5, directed = FALSE)
     plots[[i]] <- GGally::ggnet2(
       networks[[i]], edge.size = 0.3, alpha = 0.8, mode = layout_coords,
       color = color, label = F, size = 1, size.min = 0, edge.color = edge_color, title = "test"
     )
-    #+
-    #ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5, size = 6))
-
   }
 
   # Print all plots using gridExtra
